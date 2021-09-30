7d61495c0665119334062c1a1e05ac604b76327c
aimendez <aimendez@uc.cl>
2021-09-30 10:26:54 +0200

    feature function

--- main/Strategies/CrossingSMA.py
+++ main/Strategies/CrossingSMA.py
@@ -2,7 +2,6 @@ import pandas_ta as ta
 
 strategy_test = ta.Strategy(
                 name="Crossing Moving Average - https://www.investopedia.com/ask/answers/122314/how-do-i-use-moving-average-ma-create-forex-trading-strategy.asp",
-                description="SMA 50,200, BBANDS, RSI, MACD and Volume SMA 20",
                 ta=[
                     {"kind": "ema", "length": 5},
                     {"kind": "ema", "length": 20},
